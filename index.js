7b7578dec3e6610d875a3d4a8c45084ac28d92ad
Christopher Walker <cwalker@polytrope.com>
2019-04-12 19:09:50 -0400

    Additional logging for sanity check

--- index.js
+++ index.js
@@ -181,6 +181,7 @@ controller.on('slash_command', (bot, message) => {
   }
 
   if (command == '/balance') {
+    console.log(`Command is balance`)
     var pattern = /\/balance(?:\s+<@([A-z|0-9]+)\|\w+>)?/i
     var match = pattern.exec(text)
     if (match) {
