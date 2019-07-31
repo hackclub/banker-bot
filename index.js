b3ec938c4643745c99caf1e284a33b8a607eda7a
Mingjie Jiang <jiang@mingjie.info>
2019-07-31 13:52:52 -0700

    Change ignoring logic

--- index.js
+++ index.js
@@ -225,39 +225,37 @@ controller.on('slash_command', (bot, message) => {
   console.log(`Slash command received from ${user_id}: ${text}`)
   console.log(message)
 
-  if (message.channel_id == process.env.SLACK_SELF_ID) {
-    return
-  }
-
-  if (command == '/give') {
-    var pattern = /<@([A-z|0-9]+)\|.+>\s+([0-9]+)(?:gp)?(?:\s+for\s+(.+))?/
-    var match = pattern.exec(text)
-    if (match) {
-      var target = match[1]
-      var amount = match[2]
-      var note = match[3] || ''
-
-      var replyCallback = text => bot.replyPublic(message, text)
-
-      transfer(bot, 'public', user_id, target, amount, note, replyCallback)
+  if (message.channel_id != process.env.SLACK_SELF_ID) {
+    if (command == '/give') {
+      var pattern = /<@([A-z|0-9]+)\|.+>\s+([0-9]+)(?:gp)?(?:\s+for\s+(.+))?/
+      var match = pattern.exec(text)
+      if (match) {
+        var target = match[1]
+        var amount = match[2]
+        var note = match[3] || ''
+  
+        var replyCallback = text => bot.replyPublic(message, text)
+  
+        transfer(bot, 'public', user_id, target, amount, note, replyCallback)
+      }
+    }
+  
+    if (command == '/balance') {
+      var pattern = /(?:<@([A-z|0-9]+)\|.+>)?/i
+      var match = pattern.exec(text)
+      if (match) {
+        var target = match[1] || user
+        console.log(`Received balance request from User ${user} for User ${target}`)
+        getBalance(target, (balance) => {
+          var reply = user == target ?
+            `You have ${balance}gp in your account, sirrah.` :
+            `Ah yes, User <@${target}> (${target})—they have ${balance}gp.`
+          bot.replyPublic(message, reply)
+        })
+      }
     }
   }
-
-  if (command == '/balance') {
-    var pattern = /(?:<@([A-z|0-9]+)\|.+>)?/i
-    var match = pattern.exec(text)
-    if (match) {
-      var target = match[1] || user
-      console.log(`Received balance request from User ${user} for User ${target}`)
-      getBalance(target, (balance) => {
-        var reply = user == target ?
-          `You have ${balance}gp in your account, sirrah.` :
-          `Ah yes, User <@${target}> (${target})—they have ${balance}gp.`
-        bot.replyPublic(message, reply)
-      })
-    }
-
-  }
+  
 })
 
 controller.hears('.*', 'direct_mention,direct_message', (bot, message) => {
