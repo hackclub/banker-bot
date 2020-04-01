67a75d70e9a8c3db50d89e520c4929beb0f5df50
Cedric Hutchings <cedhut02@gmail.com>
2020-04-01 12:09:58 -0400

    fix grammar

--- index.js
+++ index.js
@@ -346,7 +346,7 @@ controller.hears(/pay\s+([A-z|0-9]+)/i, 'direct_mention,direct_message,bot_messa
   var replyCallback = text => {
     bot.replyInThread(message, text)
     if (typeof invoiceReplies[id] == "function") {
-      invoiceReplies[id](`<@${user}> paid their invoice of ${amount} gp from <@${target}> for "${invRecord.fields['Reason']}".`)
+      invoiceReplies[id](`<@${user}> paid their invoice of ${amount} gp from <@${target}>${invRecord.fields['Reason']}`)
     }
   };
 
