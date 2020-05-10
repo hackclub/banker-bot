bfde374765d662a2b239753878794cf033888dd3
Rishi Kothari <itsrishikothari@gmail.com>
2020-05-09 21:38:51 -0400

    BalanceCommand done

--- src/balances/balanceSelf.ts
+++ src/balances/balanceSelf.ts
@@ -1,6 +1,6 @@
-import Axios from "axios"
+import Axios, { AxiosResponse } from "axios"
 import { environment } from '../../environment';
-import { SlashCommand, SayFn } from '@slack/bolt';
+import { SlashCommand, SayFn, SayArguments } from '@slack/bolt';
 import { parseBalance } from "./parsing";
 
 const balanceRequest = async (userId: string) => {
@@ -12,6 +12,30 @@ const balanceRequest = async (userId: string) => {
 }
 
 export const balanceCommand = async (req: SlashCommand, say: SayFn) => {
-    const user = await parseBalance(req.text, req.user_id);
-    const balance = await balanceRequest(user);
+    // the ge
+    const user: string = await parseBalance(req.text, req.user_id);
+
+    // Gets the request from the database.
+    const balance: AxiosResponse<number> = (await balanceRequest(user)).data;
+    
+    await say({
+        blocks: [
+            {
+                type: "section",
+                text: {
+                    type: "mrkdwn",
+                    text: `Balance of ${user}: *${balance}* gp`
+                }
+            },
+            {
+                "type": "context",
+                "elements": [
+                    {
+                        "type": "mrkdwn",
+                        "text": `Requested by: <@${req.user_name}>`
+                    },
+                ],
+            }
+        ]
+    } as SayArguments)
 }
